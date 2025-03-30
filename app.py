c1b701b70200f75b434a8f089ad8f64b75371425
prasad1057 <khandagaleprasad188@gmail.com>
2025-03-31 01:05:10 +0530

    updated

--- app.py
+++ app.py
@@ -47,5 +47,6 @@ def toggle_task(task_id):
         db.session.commit()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
